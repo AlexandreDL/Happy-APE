18ff028b262f8d6a366a8f4df5ef5aa569e0c1c7
Isoka <isoka33@gmail.com>
2019-07-25 11:23:13 +0200

    On règles les problèmes de merge

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -2,26 +2,22 @@
 import React from 'react';
 
 // == Import : local
-<<<<<<< HEAD
+
 import NavBar from 'src/components/Includes/Nav';
 import Header from 'src/components/Includes/Header';
-=======
-import NavBar from 'src/components/Includes/Nav'
 import Footer from 'src/components/Includes/Footer'
->>>>>>> a7d52fcccd1a0ad7ecc78a271476112db25f494d
+
 import './app.scss';
 
 // == Composant
 const App = () => (
-  <Header>
-    <NavBar />
-<<<<<<< HEAD
-  </Header>
-=======
-    <div> contenu du site</div>
-    <Footer />
-  </div>
->>>>>>> a7d52fcccd1a0ad7ecc78a271476112db25f494d
+  <React.Fragment>
+    <Header>
+      <NavBar />
+    </Header>
+      <div> contenu du site</div>
+      <Footer />
+  </React.Fragment>
 );
 
 // == Export
