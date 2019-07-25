78a9373b395d97899d80b0aa8553c5d357ddfbfb
Isoka <isoka33@gmail.com>
2019-07-25 15:28:05 +0200

    Petite image pour Fabrice

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -1,11 +1,16 @@
 // == Import : npm
 import React from 'react';
+import {
+  Route, Switch,
+  Redirect, withRouter,
+} from 'react-router-dom';
 
 // == Import : local
 
 import NavBar from 'src/components/Includes/Nav';
 import Header from 'src/components/Includes/Header';
 import CGU from 'src/components/CGU';
+import HomePage from 'src/components/HomePage';
 import Footer from 'src/components/Includes/Footer';
 
 import './app.scss';
@@ -17,6 +22,8 @@ const App = () => (
       <NavBar />
     </Header>
       <main>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/conditions-generales-d-utilisation" component={CGU} />
       </main>
       <Footer />
   </React.Fragment>
