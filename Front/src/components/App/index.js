436b4eb913a5bbfb72556972a7e98b5584e6f68c
Isoka <isoka33@gmail.com>
2019-07-25 11:24:08 +0200

    Revert "React thomas"

--- Front/src/components/App/index.js
+++ Front/src/components/App/index.js
@@ -2,22 +2,13 @@
 import React from 'react';
 
 // == Import : local
-
-import NavBar from 'src/components/Includes/Nav';
-import Header from 'src/components/Includes/Header';
-import Footer from 'src/components/Includes/Footer'
-
 import './app.scss';
 
 // == Composant
 const App = () => (
-  <React.Fragment>
-    <Header>
-      <NavBar />
-    </Header>
-      <div> contenu du site</div>
-      <Footer />
-  </React.Fragment>
+  <div id="app">
+    App React
+  </div>
 );
 
 // == Export
