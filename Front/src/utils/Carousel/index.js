ee07bbe28569a20a3ed315cd97d4a523d733d153
Isoka <isoka33@gmail.com>
2019-07-26 12:53:01 +0200

    Réalisation du style de page

--- Front/src/utils/Carousel/index.js
+++ Front/src/utils/Carousel/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import AwesomeSlider from 'react-awesome-slider';
-import 'react-awesome-slider/dist/styles.css';
+import 'react-awesome-slider/src/styles';
+import SliderStyles from './slider.scss';
 
-const Slider = (
-  <p>coucou</p>
+const Slider = () => (
+  <AwesomeSlider cssModule={SliderStyles} bullets={false}>
+    <div data-src="https://gratisography.com/thumbnails/gratisography-animal-crackers-thumbnail.jpg">
+      <p>Bienvenue sur le site HAPPY APE!</p>
+    </div>
+    <div data-src="https://cdn.stocksnap.io/img-thumbs/960w/R7IOD2W1ZU.jpg">
+      <p>HAPPY APE est le site de l'Association de Parents d'Élèves de votre école</p>
+    </div>
+    <div data-src="https://picography.co/wp-content/uploads/2019/04/picography-fireworks-at-night-768x576.jpg">
+      <p>Retrouvez toutes nos actualités et nos évènements sur un seul point!</p>
+    </div>
+  </AwesomeSlider>
 );
 
 
