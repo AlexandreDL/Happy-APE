a71e0e2781466be3fbea0b5cbd94588c7fbe70ab
Isoka <isoka33@gmail.com>
2019-07-27 17:54:41 +0200

    HomePage en cours de finalisation

--- Back/symfoAPE/src/Controller/PublicController.php
+++ Back/symfoAPE/src/Controller/PublicController.php
@@ -6,18 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\News;
 use App\Repository\NewsRepository;
+use App\Entity\Event;
+use App\Repository\EventRepository;
 
 class PublicController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function home(NewsRepository $newsRepository)
+    public function home(NewsRepository $newsRepository, EventRepository $eventrepository)
     {
         $lastNewses = $newsRepository->findBy(['isPublished' => true], ['createdAt' => 'DESC'], 5, 0);
+        $nextEvent = $eventrepository->findOneBy(['isPublished' => true], ['date' => 'ASC'], 1, 0);
         return $this->json([
         'title' => 'homepage', 
-        'news' => $lastNewses
+        'news' => $lastNewses,
+        'nextEvent' => $nextEvent,
     ]);
     }
     
