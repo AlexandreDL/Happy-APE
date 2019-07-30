671a3de9442be67879d43025ef618a0f0a533283
F4brice74 <fabrice.event.manager@gmail.com>
2019-07-30 14:37:44 +0200

    nav bar done

--- Front/src/components/Includes/Header.js
+++ Front/src/components/Includes/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './header.scss';
 
 const Header = children => (
-  <header>
+  <header className="header-top">
     <img src="https://static.isodev.ovh/img/HAPPY_APE3.png" alt="img-logo-asso" center="true"/>
     {children.children}
   </header>
