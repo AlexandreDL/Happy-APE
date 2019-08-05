4c9b50a70f1c454ba9b86e54620c1b8e0cf26cdf
mariavvd <mlmuratori@yahoo.it>
2019-08-05 23:12:05 +0200

    Encode Password in progress. Not sure it is the rigth method

---
+++ Back/symfoAPE/src/Service/EncodePassword.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Service;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+
+class EncodePassword implements EventSubscriber
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoder $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$entity instanceof User) {
+            return;
+        }
+        $this->encodePassword($entity);
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$entity instanceof User) {
+            return;
+        }
+        $this->encodePassword($entity);
+        // necessary to force the update to see the change
+        $em = $args->getEntityManager();
+        $meta = $em->getClassMetadata(get_class($entity));
+        $em->getUnitOfWork()->recomputeSingleEntityChangeSet($meta, $entity);
+    }
+
+    public function getSubscribedEvents()
+    {
+        return ['prePersist', 'preUpdate'];
+    }
+
+    /**
+     * @param User $entity
+     */
+    private function encodePassword(User $user)
+    {
+        if (!$user->getPassword()) {
+            return;
+        }
+        $encoded = $this->passwordEncoder->encodePassword(
+            $user,
+            $user->getPassword()
+        );
+        $user->setPassword($encoded);
+    }
+
+}
